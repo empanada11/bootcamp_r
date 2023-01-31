6cc07550c35a891c7b9af2b7d8594343ab06c7fb
empanada11 <103653432+empanada11@users.noreply.github.com>
2023-01-31 14:02:43 +0100

    downloading counties
    counties

--- ersterAnlauf.R
+++ ersterAnlauf.R
@@ -10,4 +10,19 @@ merge_2 <- complete.cases(merge_1)
 
 areas_unique <- unique(df1$designatedArea)
 
+#####################################try the wikipedia one
+install.packages('rvest')
+library(rvest)
+# Reading in the table from Wikipedia
+page = read_html("https://en.wikipedia.org/wiki/List_of_United_States_counties_by_per_capita_income")
+# Obtain the piece of the web page that corresponds to the "wikitable" node
+my.table = html_node(page, ".wikitable")
+# Convert the html table element into a data frame
+my.table = html_table(my.table, fill = TRUE)
+# Extracting and tidying a single column from the table and adding row names
+x = as.numeric(gsub("\\[.*","",my.table[,4]))
+names(x) = gsub("\\[.*","",my.table[,2])
+# Excluding non-states and averages from the table
+per.capita.income = x[!names(x) %in% c("United States", "Northern Mariana Islands", "Guam", "American Samoa", "Puerto Rico", "U.S. Virgin Islands")]
+
 
